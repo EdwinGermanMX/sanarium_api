6433324a8573bbdb7acbecf245994f3969fda669
Edwin German <edwingermanmx@gmail.com>
2022-08-09 02:05:12 -0700

    NOW DATA IS SENT SORTED BY DATE

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -7,7 +7,7 @@ module.exports = {
     getValues: async function (req, res) {
         var { acuariumId } = req.params;
         console.log(acuariumId)
-        const data = await Data.find({acuariumId : acuariumId});
+        const data = await Data.find({acuariumId : acuariumId}).sort("-date");
         try {
           return res.status(200).json({ data });
         } catch (error) {
